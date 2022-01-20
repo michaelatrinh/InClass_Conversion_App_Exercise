2e8926daa4d26b503a19acf83d17693219473aee
mtrinh8@my.bcit.ca <mtrinh8@my.bcit.ca>
2022-01-20 15:38:50 -0800

    convert f to c, and c to f

---
+++ conversionOperations/temperature/index.js
@@ -0,0 +1,7 @@
+const celciusConvert = require('./CtoF.js');
+const fahrenheitConvert = require('./FtoC.js');
+
+module.exports = {
+  celciusConvert,
+  fahrenheitConvert
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const { celciusConvert, fahrenheitConvert } = require('./conversionOperations');
+
+console.log(celciusConvert(0));
+console.log(fahrenheitConvert(0));
