68de0544619b2acc8c5b028d3159603be6729566
Daniel Fu <danielfu123@gmail.com>
2022-01-20 16:06:22 -0800

    adding bmi and currency

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { celciusConvert, fahrenheitConvert } = require('./conversionOperations');
+const { celciusConvert, fahrenheitConvert, bmi, cadTohkd, hkdTocad} = require('./conversionOperations');
 
 console.log(celciusConvert(0));
 console.log(fahrenheitConvert(0));
+
+//First input is weight in kg, 2nd input is height in meters.
+console.log(bmi(57,1.71))
+
+console.log(cadTohkd(6));
+console.log(hkdTocad(36));
